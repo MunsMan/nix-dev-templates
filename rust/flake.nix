98c2610e86a78b628af8129f8f0f4b2291f177c6
MunsMan <munsman.github@gmail.com>
2025-04-01 10:56:39 +0200

    [rust] adding rust package build with nix

--- rust/flake.nix
+++ rust/flake.nix
@@ -18,6 +18,8 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rust = pkgs.rust-bin.stable.latest.default;
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
       in
       with pkgs;
       {
@@ -26,6 +28,11 @@
             rust
             rust-analyzer
           ];
+        };
+        packages.default = rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) version name;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
         };
       }
     );
